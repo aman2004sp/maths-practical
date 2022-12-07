b8730a39c72e16ce1b685fdbee8082fc5c515e53
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2022-12-07 10:36:56 +0530

    multipication of matrix

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
-
-arr1 = np.array([[1, 2, 3], [4, 5, 6],[7,8,9]])
-
-print(f'Original Array:\n{arr1}')
-
-arr1_transpose = arr1.transpose()
-
-print(f'Transposed Array:\n{arr1_transpose}')
+ 
+# input two matrices
+mat1 = ([1, 6, 5],[3 ,4, 8],[2, 12, 3])
+mat2 = ([3, 4, 6],[5, 6, 7],[6,56, 7])
+ 
+# This will return dot product
+res = np.dot(mat1,mat2)
+ 
+ 
+# print resulted matrix
+print(res)
