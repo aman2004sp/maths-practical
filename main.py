087b7e81b23f1eafa6f89354e320861fb6e1f2b4
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2022-12-21 10:10:42 +0530

    practicaL 2-RANK OF MATRIX AND ECHLEON FORM

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import numpy as np
+from sympy import Matrix
+  
+M = Matrix([[1, 0, 1, 3], [2, 3, 4, 7], [-1, -3, -3, -4]])
+print("Matrix : {} ".format(M))
+   
+# Use sympy.rref() method 
+M_rref = M.rref()  
+print("The Row echelon form of matrix M and the pivot columns : {}".format(M_rref))
 my_matrix = np.array([[1, 2, 1], [3, 4, 7], [3, 6, 3]])
 print(my_matrix)
 rank = np.linalg.matrix_rank(my_matrix)
-print("Rank of the given Matrix is : ",rank)
+print("Rank of the given Matrix is : ",rank)     
+
