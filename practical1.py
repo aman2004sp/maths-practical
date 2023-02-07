00cbb0f08e8e4101052d82cf4426fab1100be76b
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2023-02-07 09:58:14 +0530

    Update practical1.py

--- practical1.py
+++ practical1.py
@@ -12,3 +12,13 @@ print(my_matrix)
 rank = np.linalg.matrix_rank(my_matrix)
 print("Rank of the given Matrix is : ",rank)     
 
+
+import numpy as np
+
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(f'Original Array:\n{arr1}')
+
+arr1_transpose = arr1.transpose()
+
+print(f'Transposed Array:\n{arr1_transpose}')
